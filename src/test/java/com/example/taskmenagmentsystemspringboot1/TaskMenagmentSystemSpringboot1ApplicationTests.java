09f6a94ccb36a8cfc5c464fbe285fc0fb3772211
Arvanit Telaku <arvanit.telaku05@gmail.com>
2025-04-20 18:03:48 +0200

    Project started!

---
+++ src/test/java/com/example/taskmenagmentsystemspringboot1/TaskMenagmentSystemSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmenagmentsystemspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMenagmentSystemSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
