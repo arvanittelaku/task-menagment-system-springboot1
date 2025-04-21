77626830c2e340158c99a320b0de04f049bda708
Arvanit Telaku <arvanit.telaku05@gmail.com>
2025-04-21 22:39:48 +0200

    ViewTaskDto added!

---
+++ src/main/java/com/example/taskmenagmentsystemspringboot1/dtos/task/ViewTaskDto.java
@@ -0,0 +1,57 @@
+package com.example.taskmenagmentsystemspringboot1.dtos.task;
+
+import com.example.taskmenagmentsystemspringboot1.entities.task.TaskPriority;
+import com.example.taskmenagmentsystemspringboot1.entities.task.TaskStatus;
+import com.example.taskmenagmentsystemspringboot1.entities.user.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ViewTaskDto {
+    @NotNull(message = "Id is required")
+    @NotBlank(message = "Id is required")
+    private Long id;
+
+    @NotNull(message = "Title is required")
+    @NotBlank(message = "Title is required")
+    @Size(min = 5, message = "Title must be at least 5 characters long")
+    private String title;
+
+    @NotNull(message = "Description is required")
+    @NotBlank(message = "Description is required")
+    @Size(min = 10, message = "Description must be at least 10 characters long")
+    private String description;
+
+    @NotNull(message = "Status is required")
+    private TaskStatus status;
+
+    @NotNull(message = "Priority is required")
+    private TaskPriority priority;
+
+    @NotNull(message = "Deadline is required")
+    @NotBlank(message = "Deadline is required")
+    private LocalDate deadline;
+
+    @NotNull(message = "Created at is required")
+    @NotBlank(message = "Created at is required")
+    private LocalDate createdAt;
+
+    @NotNull(message = "Assigned to is required")
+    @NotBlank(message = "Assigned to is required")
+    private User assignedTo;
+
+    @NotNull(message = "Created by is required")
+    @NotBlank(message = "Created by is required")
+    private User createdBy;
+
+}
